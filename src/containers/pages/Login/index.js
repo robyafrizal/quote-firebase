e0aa475e4eef1e8c76172dbe9949e559b03f1202
robyafrizal <robyafrizal86@gmail.com>
2020-01-02 12:21:34 +0700

    Merge with master

--- src/containers/pages/Login/index.js
+++ src/containers/pages/Login/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import firebase from "../../../config/firebase";
 
 import { connect } from "react-redux";
 class Login extends Component {
@@ -6,6 +7,31 @@ class Login extends Component {
     this.props.changeUserName();
     this.props.changePopup();
   };
+  state = {
+    email: "",
+    password: ""
+  };
+  handleChangeText = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  handleRegisterSubmit = () => {
+    const { email, password } = this.state;
+    console.log("Data yang dikirim: ", email, password);
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log("success: ", res);
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
   render() {
     return (
       <div className="auth-container">
@@ -13,29 +39,27 @@ class Login extends Component {
           <h2 className="auth-title">
             Login Page - {this.props.popupProps} {this.props.userName}
           </h2>
-          {/* <label>Name</label>
-  <input className="input" type="text" placeholder="Your Name" /> */}
+          <center>
+            <label>Email</label>
+            <input
+              className="input"
+              type="email"
+              id="email"
+              placeholder="Your Email"
+              onChange={this.handleChangeText}
+            />
 
-          <label>Email</label>
-          <input
-            className="input"
-            type="email"
-            id="email"
-            placeholder="Your Email"
-            onChange={this.handleChangeText}
-          />
-
-          <label>Password</label>
-          <input
-            className="input"
-            type="password"
-            id="password"
-            placeholder="Your Password"
-            onChange={this.handleChangeText}
-          />
-
-          <button className="btn" onClick={this.handleRegisterSubmit}>
-            Register
+            <label>Password</label>
+            <input
+              className="input"
+              type="password"
+              id="password"
+              placeholder="Your Password"
+              onChange={this.handleChangeText}
+            />
+          </center>
+          <button className="btn" onClick={this.handleLoginSubmit}>
+            Login
           </button>
           <br />
           <br />
